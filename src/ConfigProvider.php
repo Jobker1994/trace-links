2f0a18f8e56b845022bff99f2bfee24aafdc4aef
wuhaisheng <735928019@qq.com>
2025-05-26 14:42:59 +0800

    bug

---
+++ src/ConfigProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TraceLinks;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                RedisLoggerAspect::class => \Hyperf\Di\Annotation\Inject::class,
+                SqlQueryListener::class => \Hyperf\Di\Annotation\Inject::class,
+            ],
+            'aspects' => [
+                RedisLoggerAspect::class,
+            ],
+            'listeners' => [
+                SqlQueryListener::class,
+            ],
+        ];
+    }
+}
